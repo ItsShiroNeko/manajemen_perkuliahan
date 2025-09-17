c72ca9c8e4827c9ecf489114cd7baeb7bfbcd500
Haloliza <119094377+Haloliza@users.noreply.github.com>
2025-09-17 15:10:43 +0700

    update class

--- app/Models/Khs/Khs.php
+++ app/Models/Khs/Khs.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class KHS extends Model
+class Khs extends Model
 {
     use HasFactory, SoftDeletes;
 
--- app/Models/Krs/Krs.php
+++ app/Models/Krs/Krs.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class KRS extends Model
+class Krs extends Model
 {
     use HasFactory, SoftDeletes;
 
